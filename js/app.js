bc941b36aa50da77606e78ea570c1a238419425a
ngala21 <ngalamike21@gmail.com>
2025-07-18 23:55:24 +0300

    faq section done

--- js/app.js
+++ js/app.js
@@ -18,3 +18,21 @@ navBtnDOM.addEventListener('click', () => {
 const date = getElement('#date')
 const currentYear = new Date().getFullYear()
 date.textContent = currentYear
+
+ var acc = document.getElementsByClassName("accordion");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          this.parentElement.classList.toggle("active");
+
+          var pannel = this.nextElementSibling;
+
+          if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+          } else {
+            pannel.style.display = "block";
+          }
+        });
+      }
